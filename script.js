6e4a77dc92b5db016db0f8dc40adde20d8044854
Diarrah <diarrahcasiano@gmail.com>
2020-05-17 21:27:01 -0700

    JS finished with mobile nav toggle

--- script.js
+++ script.js
@@ -1,33 +1,46 @@
+// Mobile Nav
+let menu = document.querySelector('.navbar__mobile'),
+    { body } = document;
+
+menu.addEventListener('click', function() {
+    this.classList.toggle('active')
+    this.parentElement.children[1].classList.toggle('active') // This is the navbar__links ul
+
+    this.classList.contains('active') ? body.style.overflow = 'hidden' : body.style.overflow = 'visible';
+})
+
 //Form Validation
 let form = document.querySelector('.form'),
-    email = form.querySelector('.email'),
-    formControl = form.querySelectorAll('div');
+    formControl = form.querySelectorAll('div'),
+    email = form.querySelector('.email');
 
-form.addEventListener('submit', checkInputs);
-
-function checkInputs(e) {
-    e.preventDefault();
+form.addEventListener('submit', isEmpty);
+form.addEventListener('submit', isValid);
  
-    function isEmpty() {
-        formControl.forEach(input => {
-            if (input.children[1].value.trim() === '') {
-                input.classList.add('invalid')
-                input.children[2].innerText = `This field is required`
-            } else {
-                input.classList.remove('invalid')
-            }
-        })
+function isEmpty(e) {
+    e.preventDefault();
+    formControl.forEach(input => {
+        if (input.children[1].value.trim() === '') {
+            input.classList.add('invalid')
+            input.children[2].innerText = `This field is required`
+        } else {
+            input.classList.remove('invalid')
+        }
+    })
+}
+
+function isValid(e) {
+    e.preventDefault();
+    if (!validateEmail(email.children[1].value.trim()) && email.children[1].value !== '') {
+        email.classList.add('invalid')
+        email.children[2].innerText = `Please provide a valid email`
+    } else {
+        isEmpty(email.children[1].value.trim())
     }
 
-    if (!validateEmail(email.children[1].value.trim()) && email.children[1].value !== '') {
-            email.classList.add('invalid')
-            email.children[2].innerText = `Please provide a valid email`
-        } else {
-            isEmpty(email.children[1].value.trim())
-        }
-
+    // Helper Regex function for email validation:
     function validateEmail(email) {
         let validEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return validEmail.test(String(email).toLowerCase());
-    } 
+    }
 }
